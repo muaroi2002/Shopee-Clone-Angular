8cd0f00620d62426cb9c14773e762e78f4412691
muaroi2002 <muaroi2002@gmail.com>
2024-07-23 15:09:48 +0700

    feat(navbar-wrapper): add new feature

--- shopee-angular/src/app/navbar-wrapper/navbar-wrapper.component.ts
+++ shopee-angular/src/app/navbar-wrapper/navbar-wrapper.component.ts
@@ -1,5 +1,5 @@
+import { faFacebook } from './../../../node_modules/@fortawesome/free-brands-svg-icons/index.d';
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-navbar-wrapper',
   standalone: true,
