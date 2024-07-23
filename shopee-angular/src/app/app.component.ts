649f7581b6dce3de7aa453c16dc7f99b72eccfae
muaroi2002 <muaroi2002@gmail.com>
2024-07-23 15:00:33 +0700

    refactor: import new lib

--- shopee-angular/src/app/app.component.ts
+++ shopee-angular/src/app/app.component.ts
@@ -4,11 +4,13 @@ import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { HomepageComponent } from "./homepage/homepage.component";
 import { NavbarWrapperComponent } from "./navbar-wrapper/navbar-wrapper.component";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BannerComponent } from "./banner/banner.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomepageComponent, NavbarWrapperComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomepageComponent, NavbarWrapperComponent, BannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
