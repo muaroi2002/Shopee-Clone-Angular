bf16673478fc7065e310c8520c6c67ddc2badf65
muaroi2002 <muaroi2002@gmail.com>
2024-07-24 10:25:15 +0700

    feat(route): add new routes

--- shopee-angular/src/app/app.routes.ts
+++ shopee-angular/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent,
+    title: 'Shopee',
+  },
+  {
+    path: 'product-detail/:id',
+    component: ProductDetailComponent,
+    title: 'Product Detail',
+  },
+];
